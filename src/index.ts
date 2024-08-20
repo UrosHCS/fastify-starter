65c91ffd677f971cd9fec80c6083807cece8a1a2
Uros Andjelic <uros.andjelic@nabis.com>
2024-08-20 21:48:55 +0200

    upgrade fastify

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,25 @@ const fastify = Fastify({
 
 fastify.get('/', async (request, reply) => {
   return reply.type('application/json').code(200).send({
+    id: request.id,
+    params: request.params,
     query: request.query,
+    headers: request.headers,
+    log: request.log,
+    body: request.body,
+    routeConfig: request.routeConfig,
+    routeSchema: request.routeSchema,
+    ip: request.ip,
+    ips: request.ips,
+    hostname: request.hostname,
+    url: request.url,
+    originalUrl: request.originalUrl,
+    protocol: request.protocol,
+    method: request.method,
+    routerPath: request.routerPath,
+    routerMethod: request.routerMethod,
+    routeOptions: request.routeOptions,
+    is404: request.is404,
   });
 });
 
